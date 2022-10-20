41f9ffd4d8e04583185a5001b43e02800834593a
naina-2009 <96481895+naina-2009@users.noreply.github.com>
2022-10-20 20:55:54 +0530

    Add files via upload

---
+++ HomeInterior.js
@@ -0,0 +1,54 @@
+status = "";
+objects = [];
+
+function preload()
+{
+    img = loadImage('interior.jpg');
+}
+
+function setup()
+{
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded()
+{
+    console.log("Model is successfully Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if(error)
+    {
+        console.error(error);
+    }
+    console.log(results);
+    objects = results;
+}
+
+function draw()
+{
+    image(img, 0,0, 640, 420);
+    if(status != "")
+    {
+        for(i=0; i < objects.length; i++)
+        {
+            r = random(255);
+            g = random(255);
+            b = random(255);
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("no_of_objects").innerHTML = "Number of Objects : " + objects.length;
+            fill(r,g,b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
